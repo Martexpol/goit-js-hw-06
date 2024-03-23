534679332eae895c91e5de859f25bb066608555e
Martexpol <150294627+Martexpol@users.noreply.github.com>
2024-03-23 21:26:53 +0100

    .

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,12 +1,12 @@
-const categoriesList = document.querySelectorAll('#categories .item');
+// const categoriesList = document.querySelectorAll('#categories .item');
 
-// Liczba kategorii
-const categoryCount = categoriesList.length;
-console.log('Liczba kategorii:', categoryCount);
+// // Liczba kategorii
+// const categoryCount = categoriesList.length;
+// console.log('Liczba kategorii:', categoryCount);
 
-// Dla każdej kategorii znajdź nagłówek h2 i liczbę elementów li w jej obrębie
-categoriesList.forEach(category => {
-    const categoryName = category.querySelector('h2').textContent;
-    const itemCount = category.querySelectorAll('ul > li').length;
-    console.log(`Kategoria: ${categoryName}, Liczba elementów: ${itemCount}`);
-});
+// // Dla każdej kategorii znajdź nagłówek h2 i liczbę elementów li w jej obrębie
+// categoriesList.forEach(category => {
+//     const categoryName = category.querySelector('h2').textContent;
+//     const itemCount = category.querySelectorAll('ul > li').length;
+//     console.log(`Kategoria: ${categoryName}, Liczba elementów: ${itemCount}`);
+// });
