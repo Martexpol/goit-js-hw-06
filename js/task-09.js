61eff036d1ebb0dd9ecca1c9dcbfee91086eff77
Martexpol <150294627+Martexpol@users.noreply.github.com>
2024-03-25 20:12:51 +0100

    update

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorButton = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+
+colorButton.addEventListener("click", changeColor);
+
+function changeColor () {
+ const newColor = getRandomHexColor();
+document.body.style.backgroundColor = newColor;
+colorSpan.textContent = newColor;
+}
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,29 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+const input = document.querySelector('input[type="number"]');
+
+function createBoxes (amount) {
+  const boxesDiv = document.getElementById('boxes');
+    boxesDiv.innerHTML = '';
+
+    let boxSize = 30;
+    for(i=0; i < amount; i++) {
+      const box = document.createElement('div');
+      box.style.width = boxSize + "px";
+      box.style.height = boxSize + "px";
+      box.style.backgroundColor = getRandomHexColor();
+      // boxesDiv.appendChild(box);
+      boxSize = boxSize + 10;
+    }
+}
+
+createButton.addEventListener("click", provideAmount);
+function provideAmount(input) {
+  const amount = +input.value;
+  console.log(amount);
+  createBoxes(amount);
+}
